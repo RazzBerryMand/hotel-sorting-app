47d511b3cff8434d94f8acbcdfcd8a65f44ea9cd
Mand Cashin <mandcashin@hotmail.co.uk>
2024-01-10 20:42:01 +0000

    feat: add data folder

---
+++ src/data/data.js
@@ -0,0 +1,56 @@
+import HotelImage1 from "../assets/hotel-image-1.png";
+import HotelImage2 from "../assets/hotel-image-2.png";
+import HotelImage3 from "../assets/hotel-image-3.png";
+
+export const data = [
+  {
+    id: "1",
+    name: "Iberostar Grand Salome",
+    location: "Costa Adeje, Tenerife",
+    rating: 5,
+    travellers: {
+      adults: 2,
+      children: 2,
+      infants: 1,
+    },
+    departureDate: "2019-07-03",
+    duration: "7 days",
+    departureAirport: "East Midlands",
+    price: 1136.5,
+    image: HotelImage1,
+    overview: "sgsfdsgsdfgsg",
+  },
+  {
+    id: "2",
+    name: "Aguamarina Golf Hotel",
+    location: "Costa Adeje, Tenerife",
+    rating: 4,
+    travellers: {
+      adults: 2,
+      children: 1,
+    },
+    departureDate: "2019-05-27",
+    duration: "7 days",
+    departureAirport: "Liverpool",
+    price: 696.8,
+    image: HotelImage2,
+    overview: "sgsfdsgsdfgsg",
+  },
+  {
+    id: "3",
+    name: "Las Piramides Resort",
+    location: "Costa Adeje, Tenerife",
+    rating: 3,
+    travellers: {
+      adults: 2,
+      children: 2,
+    },
+    departureDate: "2019-07-0318",
+    duration: "7 days",
+    departureAirport: "Manchester",
+    price: 499.99,
+    image: HotelImage3,
+
+    overview: "sgsfdsgsdfgsg",
+  },
+];
