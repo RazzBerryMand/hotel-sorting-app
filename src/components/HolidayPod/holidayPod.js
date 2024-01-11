e1debfaaf07b5939206b040057357f91b6d8c9cd
Mand Cashin <mandcashin@hotmail.co.uk>
2024-01-11 21:26:42 +0000

    feat: add overview panel functionality

--- src/components/HolidayPod/holidayPod.js
+++ src/components/HolidayPod/holidayPod.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Travellers } from "../Travellers/travellers";
 import { TripDates } from "../TripDates/tripDates";
 import { StarRating } from "../StarRating/starRating";
 import { BookNowCta } from "../BookNowCta/bookNowCta";
+import { FaChevronDown } from "react-icons/fa";
+import { FaChevronRight } from "react-icons/fa";
 
 export const HolidayPod = ({
   name,
@@ -15,12 +17,39 @@ export const HolidayPod = ({
   image,
   overview,
 }) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  const handleShowInfo = () => {
+    setShowInfo(!showInfo);
+  };
+
   return (
     <article className="bg-white text-slate-600">
       <div className="md:flex">
         <div className="overflow-hidden md:w-4/6 relative">
           <img className="object-cover h-full w-full" src={image} alt={name} />
-          <button>Read more about this hotel</button>
+          <button
+            onClick={handleShowInfo}
+            className="cursor-pointer absolute bottom-0 z-10 bg-white py-2 px-4 text-blue-800 flex"
+          >
+            {showInfo ? (
+              <div className="flex items-center">
+                <span className="font-bold mr-1">Read less</span>
+                about this hotel
+                <span className="ml-3">
+                  <FaChevronDown />
+                </span>
+              </div>
+            ) : (
+              <div className="flex items-center">
+                <span className="font-bold mr-1">Read more</span>
+                about this hotel
+                <span className="ml-3">
+                  <FaChevronRight />
+                </span>
+              </div>
+            )}
+          </button>
         </div>
         <div className="p-4 md:w-2/6">
           <div className="pb-2">
@@ -39,8 +68,15 @@ export const HolidayPod = ({
           <BookNowCta price={price} />
         </div>
       </div>
-      {/* TODO: finish overview panel show/hide functionality */}
-      <div>{overview}</div>
+
+      {showInfo && (
+        <div className="p-4">
+          <h3 className="text-blue-800 pb-4">
+            <strong>Overview</strong>
+          </h3>
+          <p>{overview}</p>
+        </div>
+      )}
     </article>
   );
 };
